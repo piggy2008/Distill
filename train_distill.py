eab9b63e09a636e6dd91a3bd2df343a58e5331e5
piggy2008 <810656271@qq.com>
2020-02-02 20:29:14 +0800

    parameter update

--- train_distill.py
+++ train_distill.py
@@ -41,7 +41,7 @@ args = {
     'iter_num': 80000,
     'iter_save': 10000,
     'iter_start_seq': 0,
-    'train_batch_size': 8,
+    'train_batch_size': 6,
     'last_iter': 0,
     'lr': 1e-3,
     'lr_decay': 0.9,
