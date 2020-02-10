175ae1fbef114db079095721411ae7b82082d288
piggy <810656271@qq.com>
2020-02-10 09:58:52 +0800

    update

--- train_distill.py
+++ train_distill.py
@@ -20,7 +20,7 @@ from utils_mine import load_part_of_model
 import random
 
 cudnn.benchmark = True
-device_id = 3
+device_id = 2
 torch.manual_seed(2019)
 torch.cuda.set_device(device_id)
 
@@ -38,18 +38,18 @@ args = {
     'distillation': False,
     'L2': False,
     'KL': False,
-    'iter_num': 80000,
+    'iter_num': 30000,
     'iter_save': 10000,
     'iter_start_seq': 90000,
     'train_batch_size': 8,
     'last_iter': 0,
-    'lr': 1e-3,
+    'lr': 1e-5,
     'lr_decay': 0.9,
     'weight_decay': 5e-4,
     'momentum': 0.95,
     'snapshot': '',
-    # 'pretrain': os.path.join(ckpt_path, 'VideoSaliency_2019-12-24 22:05:11', '50000.pth'),
-    'pretrain': '',
+    'pretrain': os.path.join(ckpt_path, 'VideoSaliency_2019-12-24 22:05:11', '70000.pth'),
+    # 'pretrain': '',
     'imgs_file': 'Pre-train/pretrain_all_seq_DUT_TR_DAFB2_DAVSOD2.txt',
     # 'imgs_file': 'video_saliency/train_all_DAFB2_DAVSOD_5f.txt',
     'train_loader': 'both'
